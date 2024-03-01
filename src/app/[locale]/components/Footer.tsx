a6f8e78e976c7095181990ed29a7d128bb4c6313
IT-Developer <itdeveloper.coder@gmail.com>
2024-03-02 00:27:47 +0530

    footer color white

--- src/app/[locale]/components/Footer.tsx
+++ src/app/[locale]/components/Footer.tsx
@@ -80,7 +80,7 @@ type FooterColumnProps = {
 const FooterColumn = ({ title, children }: FooterColumnProps) => {
   return (
     <div className="flex flex-col gap-5">
-      <h4 className="bold-18 whitespace-nowrap">{title}</h4>
+      <h4 className="bold-18 text-white whitespace-nowrap ">{title}</h4>
       {children}
     </div>
   )
